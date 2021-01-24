396bfaa149aff553dbf45e668f49fa0b462fcd96
nutchapon hanouypornlert <nutchapon@enres.co>
2021-01-24 11:28:35 +0700

    custom header

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hello Bom');
-})
+app.get("/", (req, res) => {
+  res.send("Hello Bom");
+});
+
+app.get("/demo", () => {
+  res
+    .status(418)
+    .set("X-full-stack", "4life")
+    .send("I show demo custom header");
+});
 
 app.listen(3000, () => {
-	console.log('server is runing at 3000')
+  console.log("server is runing at 3000");
 });
